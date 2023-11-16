e64e9d6d7048973527708f97030f024318881ecc
shashank <125953781+shashankk12345@users.noreply.github.com>
2023-11-16 13:52:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const no1 = parseFloat(prompt("enter first no"))
+// const operator = prompt('choose a operator');
+// const no2 = parseFloat(prompt("enter second no"))
+
+
+// switch (operator){
+//     case '+': alert(no1+no1);
+//     break;
+//     case '-': alert(no1-no2);
+//     break;
+//     case '*': alert(no1*no2);
+//     break;
+//     case '/': alert(no1/no2);
+//     default: 'choose a prior operator';
+// };
+
